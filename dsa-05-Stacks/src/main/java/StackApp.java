095b11f8db0f3410510378b99b72d8f79f7ac292
Adelina <adelina.abdykanyeva@gmail.com>
2022-10-13 06:27:51 -0500

    Stack

---
+++ dsa-05-Stacks/src/main/java/StackApp.java
@@ -0,0 +1,18 @@
+public class StackApp {
+
+    public static void main(String[] args) {
+
+        MyStack<Integer> stack = new MyStack<Integer>();
+
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+        }
+
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(stack.pop());
+        }
+    }
+}
